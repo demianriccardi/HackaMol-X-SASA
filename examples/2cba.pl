763a66cf128736e58c2bb3e8bc2aefb03065dfef
Demian Riccardi <demianriccardi@gmail.com>
2016-04-08 11:04:59 -0400

    simplified example

--- examples/2cba.pl
+++ examples/2cba.pl
@@ -4,8 +4,7 @@ use HackaMol::X::SASA;
 
 
 my $mol  = HackaMol -> new() -> pdbid_mol('2cba');
-#my $sasa = HackaMol::X::SASA->new( scratch => "CAII");
-my $sasa = HackaMol::X::SASA->new();
+my $sasa = HackaMol::X::SASA->new( scratch => "CAII");
 
 my $mol_sasa = $sasa->calc_mol($mol);
 $sasa->print_summary; 
